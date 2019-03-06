281c07bacb4f2820a1786b36680588f10173b503
Filipowicz.Wiktor <209309@edu.p.lodz.pl>
2019-03-06 19:04:46 +0100

    Próba poprawy formatowania

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,17 +1,20 @@
 package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        if (value == null)
-            return new ArrayList<>();
+        if (value == null) {
+            return Collections.emptyList();
+        }
 
         ArrayList<String> result = new ArrayList<>();
-        for (int i = 0; i < value.length() + 1; i++)
+        for (int i = 0; i < value.length() + 1; i++) {
             result.add(value.substring(i));
+        }
 
         return result;
     }
